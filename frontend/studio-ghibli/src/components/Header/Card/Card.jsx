70deb544d00922953da497f2c4d0d3aab5cbb73c
L00179208 <L00179208@atu.ie>
2024-02-04 15:55:07 +0000

    Text Content changed to check the Pipeline

--- frontend/studio-ghibli/src/components/Header/Card/Card.jsx
+++ frontend/studio-ghibli/src/components/Header/Card/Card.jsx
@@ -2,7 +2,7 @@ import { CardContainer, TitleContainer, TextContainer } from "./Card.styles";
 const Card = () => {
   return (
     <CardContainer>
-      <TitleContainer>What's your dream so far?</TitleContainer>
+      <TitleContainer>Our New Course is Coming Soon</TitleContainer>
       <TextContainer>
         Log in now and learn the skills to help make it happen.
       </TextContainer>
