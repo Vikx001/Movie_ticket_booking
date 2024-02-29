b6a65e393e9ea213eeb12449e6e3f36e67ab5a85
ShreyaRaghuvanshi1 <145033750+ShreyaRaghuvanshi1@users.noreply.github.com>
2024-02-29 14:36:27 +0000

    Update main.jsx

--- admin_frontend/src/main.jsx
+++ admin_frontend/src/main.jsx
@@ -9,7 +9,7 @@
 =========================================================
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
-import React from "react";
+import React from "react"; 
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
