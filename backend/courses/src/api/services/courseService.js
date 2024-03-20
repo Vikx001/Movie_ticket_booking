1d11e09b17eac78ec7acbc36c8f9a5d841c8b4ba
Aneesh Ramakrishna Pillai <145033744+L00179208@users.noreply.github.com>
2024-03-20 14:25:34 +0000

    Update courseService.js

--- backend/courses/src/api/services/courseService.js
+++ backend/courses/src/api/services/courseService.js
@@ -118,7 +118,6 @@ class CourseService {
       const courses = await Course.findAll(options);
       return courses;
     } catch (error) {
-      res.status(500).json({ message: error.message });
       throw new Error(error.message);
     }
   }
